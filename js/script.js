3b5a66ecfc3d6048dfa236bf8af2d1dae610c387
PabloMurillo <murillomatapabloesteban@gmail.com>
2022-07-11 14:00:23 -0500

    Cambio de color respescto al menu

--- js/script.js
+++ js/script.js
@@ -8,11 +8,39 @@ $(document).ready(function(){
             console.log(catProduct);
 
             //AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO ================
-            $('.categoryItem').removeClass('ct_itemActive');
-            $(this).addClass('ct_itemActive');
+            //$('.categoryItem').removeClass('ct_itemActive');
+            //$(this).addClass('ct_itemActive');
 
             //OCULTANDO PRODUCTOS =============================================
-            $('.product-item').hide();
+            $('.product-item').hide();  
+            //CAMBIAMOS COLOR DEL FONDO =======================================
+            switch(catProduct) {
+                case 'art':
+                    $("body").css("background-color",'#98266d');
+                     break;
+                case 'emprende':
+                    $("body").css("background-color",'#ec7715');
+                  break;
+                  case 'social':
+                    $("body").css("background-color",'#86c4e2');
+                  break;
+                  case 'tech':
+                    $("body").css("background-color",'#3f5485');
+                  break; 
+                  case 'eco':
+                    $("body").css("background-color",'#3fa93b');
+                  break;
+                  case 'all':
+                    $("body").css("background-color",'#2d3e50');
+                  break;
+                  default:
+                    $("body").css("background-color",'#2d3e50');      
+                  // code block
+              }
+                
+            
+            
+            
 
             //MOSTRANDO PRODUCTOS =============================================
             $('.product-item[category="'+catProduct+'"]').show();
